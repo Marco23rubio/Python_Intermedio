a577646f930b5997a080fd59236e2bff3a04ee84
Marco Rubio <marcoantonio23rubio@gmail.com>
2022-01-26 20:40:48 -0700

    Le agrugue a Debuggin.py el uso del try,raise y excepts para el manejo de errores a la hora de solocitar un numero al usuario

--- debuggin.py
+++ debuggin.py
@@ -1,14 +1,20 @@
 def divisor(num):
-    divisors = []
-    for i in range(1,num+1):
-        if num % i == 0:
-            divisors.append(i)
-    return divisors
-
+    
+    try:
+        lista =[i for i in range(1,num+1)if num %i ==0]
+        if num <=0:
+            raise ValueError("No se pueden ingresar numeros negativos o el 0")        
+        return lista              
+    except ValueError as ve:
+        print(ve)
+        return str(num) + ":No es un numero positivo"
+        
 def run():
-    num = int(input("Ingresa un número: "))
-    print(divisor(num))
-    print("Termino mi programa")
+    try:
+        num = int(input("Ingresa un número: "))    
+        print(divisor(num))
+    except ValueError:
+        print("Solo se pueden ingresar números enteros")
 
 
 if __name__ == "__main__":
